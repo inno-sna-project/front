ade9ac6107a5f6c663d3d0b5543baf98e3d02d88
Pavel Baharuev <pavel5609s@gmail.com>
2022-12-13 18:57:10 +0300

    #feature: rename methods

--- my-app/src/services/UserService.js
+++ my-app/src/services/UserService.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 
 export async function getAllUsers() {
 
-    const response = await axios.get('/api/users');
+    const response = await axios.get('/api/dropped_students');
     return response.data;
 }
 
 export async function createUser(data) {
-    const response = await axios.post(`/api/user`, {user: data});
+    const response = await axios.post(`/api/add_dropped_student`, {user: data});
     return response.data;
 }
