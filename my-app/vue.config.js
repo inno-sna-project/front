98dbb343b61fbea9e1d946ebec0ffef4985b4022
TeaWhyDee <daniel.555365@gmail.com>
2022-12-13 19:19:34 +0300

    config change

--- my-app/vue.config.js
+++ my-app/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '^/api': {
-          target: 'https://snaback.teawide.xyz',
+          target: 'https://snaback.teawide.xyz/api/dropped_students',
           changeOrigin: true
         },
       }
